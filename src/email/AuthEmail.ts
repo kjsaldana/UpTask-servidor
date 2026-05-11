cb9ee33b93a059c5d8e3a261c138bf3dc1cadf8d
Kevin J <kjoseph1910@hotmail.com>
2026-05-11 20:40:06 +0200

    feat: add authEmail and passwordReset code

--- src/email/AuthEmail.ts
+++ src/email/AuthEmail.ts
@@ -1,4 +1,4 @@
-import { transporter } from "../config/nodemailer";
+import { resend } from "../config/resend";
 
 interface IUser {
     email: string;
@@ -8,36 +8,42 @@ interface IUser {
 
 export class AuthEmail {
     static sendEmail = async (user: IUser) => {
-        const info = await transporter.sendMail({
-            from: 'Uptask <admin@uptask.com>',
-            to: user.email,
+        const { data, error } = await resend.emails.send({
+            from: 'Uptask <onboarding@resend.dev>', // Cambia esto por tu dominio verificado
+            to: [user.email],
             subject: 'Uptask - confirma tu cuenta',
-            text: 'Uptask - confirma tu cuenta',
             html: `
                 <p>Hola, ${user.name}</p>
-                <p>Te damos la bienvenida a UpTask, vistita el siguiente enlace para confirmar tu cuenta:</p>
+                <p>Te damos la bienvenida a UpTask, visita el siguiente enlace para confirmar tu cuenta:</p>
                 <a href="${process.env.FRONTEND_URL}/confirmar-cuenta">Confirmar cuenta aquí</a>
                 <p>Ingresa el siguiente código: <b>${user.token}</b></p>
                 <p>Nota: el código expirará en 10 minutos.</p>
             `
-        })
-        console.log('Enviado', info.messageId);
+        });
+
+        if (error) {
+            return console.error(error);
+        }
+        console.log('Email enviado con éxito', data?.id);
     }
 
     static sendPasswordResetCode = async (user: IUser) => {
-        const info = await transporter.sendMail({
-            from: 'Uptask <admin@uptask.com>',
-            to: user.email,
+        const { data, error } = await resend.emails.send({
+            from: 'Uptask <onboarding@resend.dev>', // Cambia esto por tu dominio verificado
+            to: [user.email],
             subject: 'Uptask - Restablecer contraseña',
-            text: 'Uptask - Restablecer contraseña',
             html: `
                 <p>Hola, ${user.name}</p>
-                <p>Para continuar con el proceso vistita el siguiente enlace:</p>
+                <p>Para continuar con el proceso visita el siguiente enlace:</p>
                 <a href="${process.env.FRONTEND_URL}/acceso/nueva-contrasena">Restablecer Contraseña</a>
                 <p>Ingresa el siguiente código: <b>${user.token}</b></p>
                 <p>Nota: el código expirará en 10 minutos.</p>
             `
-        })
-        console.log('Enviado', info.messageId);
+        });
+
+        if (error) {
+            return console.error(error);
+        }
+        console.log('Email de reset enviado', data?.id);
     }
 }
